c3d2ae1011f4878b5d614096afd224d62dcd1cbb
Jordan-deJong <jordan.dejong@me.com>
2015-10-06 13:25:07 +1100

    importer class

---
+++ lib/scaffolding/parser/importer/datdata.rb
@@ -0,0 +1,26 @@
+module Scaffolding
+  module Parser
+    module Importer
+      class DatData < Scaffolding::Parser::Raw
+
+        def initialize(file="", auto)
+          super
+          @saved = 0
+          @failed = 0
+        end
+
+        def process_row(row)
+           model = @source_name.classify.constantize.new(row.except!(:id))
+           model.save ? @saved += 1 : @failed += 1
+        end
+
+        def results
+          process_data
+          return @errors unless @errors.count == 0
+          {saved: @saved, failed: @failed}
+        end
+
+      end
+    end
+  end
+end
