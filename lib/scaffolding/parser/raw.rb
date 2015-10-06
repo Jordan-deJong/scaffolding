33d65d40db76306719984d5fdf174560b7004309
Jordan-deJong <jordan.dejong@me.com>
2015-10-06 17:44:50 +1100

    raw class

--- lib/scaffolding/parser/raw.rb
+++ lib/scaffolding/parser/raw.rb
@@ -59,5 +59,8 @@ module Scaffolding
       end
 
     end
+    raw_class = Class.new(Scaffolding::Parser::Raw) do; end
+    Scaffolding::Parser.const_set("Dat", raw_class)
+    Scaffolding::Parser.const_set("Txt", raw_class)
   end
 end
--- lib/scaffolding/parser/rawclasses.rb
+++
@@ -1,4 +0,0 @@
-raw_class = Class.new(Scaffolding::Parser::Raw) do
-end
-Scaffolding::Parser.const_set("Dat", raw_class)
-Scaffolding::Parser.const_set("Txt", raw_class)
